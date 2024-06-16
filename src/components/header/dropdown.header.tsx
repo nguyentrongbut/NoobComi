76d3932e02bd1f4ef635549642aeb214c19431b0
Bút <Cloly031128@gmail.com>
2024-06-17 01:56:05 +0700

    feat: Add header component to the layout

---
+++ src/components/header/dropdown.header.tsx
@@ -0,0 +1,100 @@
+"use client";
+import IconArrowDown from "@/components/icon/icon.arrow.down";
+import IconCup from "@/components/icon/icon.cup";
+import IconLight from "@/components/icon/icon.light";
+import IconMoney from "@/components/icon/icon.money";
+import IconPage from "@/components/icon/icon.page";
+import IconUpload from "@/components/icon/icon.upload";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+
+// Create a list of dropdown(hover) "More" navbar links to display
+const navMoreLinks = [
+    {
+        title: "Creators",
+        icon: IconLight,
+        desc: "Build your fandom on MangaNoob",
+        href: "/creators",
+    },
+    {
+        title: "News",
+        icon: IconPage,
+        desc: "The latest on anime, manga and games",
+        href: "/news",
+    },
+    {
+        title: "Contests",
+        icon: IconCup,
+        desc: "Monthly contests, big prizes",
+        href: "/contests",
+    },
+    {
+        title: "Publish",
+        icon: IconUpload,
+        desc: "Unlock your potential with MangaNoob",
+        href: "/publish",
+    },
+    {
+        title: "Make Money",
+        icon: IconMoney,
+        desc: "Earn money with MangaNoob",
+        href: "/makemoney",
+    },
+];
+
+const DropdownHeader = () => {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem className="hover:bg-neutral-200 rounded-md">
+                    <NavigationMenuTrigger className="flex text-base font-medium px-4 py-2 cursor-pointer bg-transparent">
+                        <div className="flex gap-1">
+                            <span>More</span>
+                            <IconArrowDown></IconArrowDown>
+                        </div>
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <div className="w-[448px]">
+                            {/* Map through the navMoreLinks array to display each dropdown link */}
+                            {navMoreLinks.map((navMoreLink) => {
+                                // Assign the icon in list to LinkIcon
+                                const LinkIcon = navMoreLink.icon;
+                                return (
+                                    <div
+                                        key={navMoreLink.desc}
+                                        className="line-clamp-1"
+                                    >
+                                        <Link
+                                            href={navMoreLink.href}
+                                            className="group flex gap-4 items-center py-2 px-4 cursor-pointer hover:bg-neutral-100 transition"
+                                        >
+                                            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-neutral-50 group-hover:bg-white transition">
+                                                <LinkIcon></LinkIcon>
+                                            </div>
+                                            <div>
+                                                <span className="font-semibold">
+                                                    {navMoreLink.title}
+                                                </span>
+                                                <p className="text-neutral-500 mt-1 font-medium">
+                                                    {navMoreLink.desc}
+                                                </p>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    );
+};
+
+export default DropdownHeader;
---
+++ src/components/icon/icon.sign.up.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const IconSignUp = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="mr-3 flex-shrink-0"
+            aria-hidden="true"
+        >
+            <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"></path>
+            <path d="M13.5 6.5l4 4"></path>
+        </svg>
+    );
+};
+
+export default IconSignUp;
