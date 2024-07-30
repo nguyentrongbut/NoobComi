2c7a240a4fdc9bd1710ece59cde56c0046c78aa1
Bút <Cloly031128@gmail.com>
2024-07-30 21:51:08 +0700

    fix(icon): Convert attribute from svg to tsx

--- src/components/icon/icon.arrow.prev2.tsx
+++ src/components/icon/icon.arrow.prev2.tsx
@@ -10,9 +10,9 @@ const IconArrowPrev2 = (props: ComponentProps<"svg">) => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width={2}
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
             {...props}
         >
             <path d="M5 12l14 0"></path>
