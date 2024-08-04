ec2ddd8a7f5449e3c7ba60b643541536595a732a
Bút <Cloly031128@gmail.com>
2024-08-05 01:24:29 +0700

    feat: ratting

--- src/app/title/[slug]/reviews/page.tsx
+++ src/app/title/[slug]/reviews/page.tsx
@@ -1,5 +1,12 @@
+import TabContent from "@/app/title/@component/tab.content.wrapper";
+import Ratting from "@/app/title/[slug]/reviews/@component/ratting";
+
 const ReviewsTab = () => {
-    return <div>this is reviews tab</div>;
+    return (
+        <TabContent>
+            <Ratting></Ratting>
+        </TabContent>
+    );
 };
 
 export default ReviewsTab;
