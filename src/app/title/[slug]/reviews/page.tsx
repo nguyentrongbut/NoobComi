7ab70ea436242b817f46d7ff9c44fd6ff542fdda
Bút <Cloly031128@gmail.com>
2024-08-05 10:17:05 +0700

    feat(ui): add ui review form

--- src/app/title/[slug]/reviews/page.tsx
+++ src/app/title/[slug]/reviews/page.tsx
@@ -1,10 +1,26 @@
 import TabContent from "@/app/title/@component/tab.content.wrapper";
 import Ratting from "@/app/title/[slug]/reviews/@component/ratting";
+import IconSend from "@/components/icon/icon.send";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 const ReviewsTab = () => {
     return (
         <TabContent>
             <Ratting></Ratting>
+            <div className="relative">
+                <Textarea className="min-h-[7rem] mt-4 bg-neutral-100 border border-[#a3a3a3] focus:border-[var(--primary-color)] focus:outline-none transition-all peer"></Textarea>
+                <label className="absolute top-2 left-4 text-neutral-700 transition-all rounded text-base pointer-events-none peer-focus-within:text-xs peer-focus-within:-top-2 peer-focus-within:bg-white px-1 -mx-1 max-w-[calc(100% - 2rem)]">
+                    <span className="relative z-[2] overflow-hidden whitespace-nowrap">Write a review</span>
+                    <div className="absolute left-0 bottom-0 w-full h-1/2 z-[1] bg-neutral-100"></div>
+                </label>
+            </div>
+            <div className="flex justify-end mt-6">
+                <Button className="flex gap-1 items-center bg-primary-color p-1.5">
+                    <IconSend></IconSend>
+                    <span>Submit</span>
+                </Button>
+            </div>
         </TabContent>
     );
 };
---
+++ src/components/icon/icon.send.tsx
@@ -0,0 +1,24 @@
+import { ComponentProps } from "react";
+
+const IconSend = (props: ComponentProps<"svg">) => {
+    return (
+        <svg
+            data-v-b8aef6c3=""
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+        >
+            <path d="M10 14l11 -11"></path>
+            <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5"></path>
+        </svg>
+    );
+};
+
+export default IconSend;
