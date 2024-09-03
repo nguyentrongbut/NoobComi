d141d2611aaa04090776da1f93eadc78b292e125
Bút <Cloly031128@gmail.com>
2024-09-03 12:08:23 +0700

    fix: fix logic follow and unfollow

--- src/app/title/@component/btn.toggle.follow.tsx
+++ src/app/title/@component/btn.toggle.follow.tsx
@@ -16,25 +16,12 @@ const BtnToggleFollow = ({
     totalFollow: number;
     followBy: number[];
 }) => {
-    const [follow, setFollow] = useState(true);
+    const userId = 9; 
     const router = useRouter();
-    const userId = 9;
+    const [follow, setFollow] = useState(followBy.includes(userId));
 
     const handleFollowToggle = async () => {
         if (follow) {
-            const followed = await sendRequest<ITopComics[]>({
-                url: `${process.env.NEXT_PUBLIC_WEB_COMIC_API}/api/comics/${id}`,
-                method: "PATCH",
-                body: {
-                    follow: totalFollow + 1,
-                    followBy: [...followBy, userId],
-                },
-            });
-            if (followed) {
-                setFollow(false);
-                router.refresh();
-            }
-        } else {
             const unfollowed = await sendRequest<ITopComics[]>({
                 url: `${process.env.NEXT_PUBLIC_WEB_COMIC_API}/api/comics/${id}`,
                 method: "PATCH",
@@ -44,29 +31,37 @@ const BtnToggleFollow = ({
                 },
             });
             if (unfollowed) {
+                setFollow(false);
+                router.refresh();
+            }
+        } else {
+            const followed = await sendRequest<ITopComics[]>({
+                url: `${process.env.NEXT_PUBLIC_WEB_COMIC_API}/api/comics/${id}`,
+                method: "PATCH",
+                body: {
+                    follow: totalFollow + 1,
+                    followBy: [...followBy, userId],
+                },
+            });
+            if (followed) {
                 setFollow(true);
                 router.refresh();
             }
         }
     };
 
-    return follow ? (
+    return (
         <Button
-            className="flex bg-primary-color hover:opacity-90 sm:w-[228.5625px] font-medium sm:text-base p-2 sm:p-0"
+            className={`flex ${
+                follow ? "bg-[#e5e5e5] hover:bg-[#b2b2b2] text-black" : "bg-primary-color hover:opacity-90 text-white"
+            } sm:w-[228.5625px] font-medium sm:text-base p-2 sm:p-0`}
             onClick={handleFollowToggle}
         >
-            <IconSave />
-            <span className="ml-1">Follow</span>
-        </Button>
-    ) : (
-        <Button
-            className="flex bg-[#e5e5e5] hover:bg-[#b2b2b2] sm:w-[228.5625px] font-medium sm:text-base p-2 sm:p-0 text-black"
-            onClick={handleFollowToggle}
-        >
-            <IconUnSave />
-            <span className="ml-1">Unfollow</span>
+            {follow ? <IconUnSave /> : <IconSave />}
+            <span className="ml-1">{follow ? "Unfollow" : "Follow"}</span>
         </Button>
     );
 };
 
 export default BtnToggleFollow;
+
